61da3e41cb344d519c79926b19147c8a1ffd6b75
AXOT <antonzayats55@gmail.com>
2020-12-06 16:24:47 +0200

    contracts

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import abi from './artifacts/Ballot.json'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const { ethers } = require('ethers');
 var contract = require("@truffle/contract");
 //const fs = require('fs');
 const path = require('path');
@@ -54,7 +55,7 @@ function SelectCandidate() {
                       <input type="date" className="form-control" id="date_candidate" placeholder="Введите дату рождения кандидата"/>
                       <small id="date_candidate" className="form-text text-muted">Дата рождения кандидата</small>
                   </div>                                         
-                  <button type="submit" class="btn btn-primary">Зарегистрировать</button>
+                  <button type="submit" class="btn btn-primary" disabled="disabled">Зарегистрировать</button>
                 </form>
           </div>
       </div>
@@ -92,39 +93,45 @@ window.web3 = new web3(window.web3.currentProvider);
 
 const ethereumButton = document.querySelector('.enableEthereumButton');
 
-ethereumButton.addEventListener('click', async () => {
+ethereumButton.addEventListener('click', async (e) => {
+  e.preventDefault();
   //Will Start the metamask extension 
-  alert("123");
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  alert("s123");
+  // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  //const account = accounts[0];
+  const account = window.ethereum.selectedAddress;
 // Compile the source code
 // const input = require(path.resolve(__dirname, 'client/src/artifacts', 'Ballot.json'));
-alert(1);
 
 try {
   const newcontracter = new window.web3.eth.Contract(abi.abi, "0xbF25484776f7Df855A5a15DA7513D11072373Fd7");
 
 // newcontracter.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:9545/'));
-alert(3);
-alert(6);
 //
-console.log(newcontracter.methods)
-alert(6);
-console.log(newcontracter.methods)
-
-// newcontracter.methods.vote(1).send({from : accounts[0]}).then(console.log).catch(console.log);
+let failed = false;
+try {
+  const x1 = new ethers.utils.BigNumber(1);
+  let result = await newcontracter.methods.vote("1").send({from : account});
+  //alert('success');
+} catch (e) {
+  failed = true;
+  //alert(`Error: ${e.message}`);
+}
+/* await newcontracter.methods.vote(1).send({from : account}).then((...args) => console.log('success', ...args)).catch((...args) => {
+  failed = true;
+  console.log('error', ...args);
+});
+alert(`x1: ${failed}`); */
+if (failed) {
+  //throw new Error();
+}
 // alert(res)
 let res = await newcontracter.methods.winningProposal().call();
-alert(res);
 
 // alert(res);
 
 } catch(e) {
-  console.log(e)
+  //console.log(e)
 }
-alert(2);
-alert(2);
 
 });
 }
